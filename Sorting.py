d45d81b752f7976b10f5f390d69333c64e879e5b
Sargam Shah <sargam5360@gmail.com>
2017-07-08 11:49:31 -0500

    Add files via upload

---
+++ Sorting.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[16]:
+
+# Sorting
+
+# Bubble Sort
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [54,26,93,17,77,31,44,55,20]
+bubbleSort(alist)
+print(alist)
+ 
+    
+
+                 
+alist = [54,22,98,17,99,23,55,23,88]
+bubbleSort(alist)
+print(alist)
+
