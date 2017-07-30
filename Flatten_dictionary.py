4dc8ae93eb19a647b2c7c156d98bf23825b21da1
Sargam Shah <sargam5360@gmail.com>
2017-07-30 15:31:24 -0500

    Update Flatten_dictionary.py

--- Flatten_dictionary.py
+++ Flatten_dictionary.py
@@ -1,1 +1,34 @@
+def flattenDictionary(dict):
+    flatDictionary = {}
+    flattenDictionaryHelper("", dict, flatDictionary)
 
+    return flatDictionary
+
+
+def flattenDictionaryHelper(initialKey, dict, flatDictionary):
+    for key in dict:
+        value = dict.get(key)
+
+        if type(value) != type(dict): # the value is of a primitive type
+            if (initialKey == None or initialKey == ""):
+                flatDictionary[key] = value
+            else:
+                flatDictionary[initialKey + "." + key] =  value
+        else:
+        	if (initialKey == None or initialKey == ""):
+                    flattenDictionaryHelper(key, value, flatDictionary)
+    		else:
+                    flattenDictionaryHelper(initialKey + "." + key, value, flatDictionary)
+            
+ dict = {
+            "Key1" : "1",
+            "Key2" : {
+                "a" : "2",
+                "b" : "3",
+                "c" : {
+                    "d" : "3",
+                    "e" : "1"
+                }
+            }
+        }
+ flattenDictionary(dict)
